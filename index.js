361f15820120e89b8f274511e96c9a08f4c4e021
Vitus Balazs <Balazs.Vitus@msg.group>
2024-07-15 15:44:18 +0300

    Chapter 2: Added debug script to package.json

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log('hello world');
+console.log('hello world1');
+console.log('hello world2');
+let unusedVariable = 5;
+console.log('hello world3');
+const UNUSED_CONST = 'this is a string';
+console.log('hello world4');
