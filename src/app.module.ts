16ad490477788ac8564db91795626f2d8589b6a3
Vitus Balazs <Balazs.Vitus@msg.group>
2024-07-16 10:58:44 +0300

    Chapter 3: Added feature modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { HealthController } from './health/health.controller';
+import { HealthController } from './health.controller';
+import { OrdersModule } from './orders/orders.module';
+import { ProductsModule } from './products/products.module';
+import { CustomersModule } from './customers/customers.module';
+import { SharedModule } from './shared/shared.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, HealthController],
-  providers: [AppService],
+  imports: [OrdersModule, ProductsModule, CustomersModule, SharedModule],
+  controllers: [HealthController],
 })
 export class AppModule {}
