40938ac514c7e97a7f9a4db44544ea358b5d2205
Vitus Balazs <Balazs.Vitus@msg.group>
2024-07-16 14:51:34 +0300

    Practice paused at this point, commiting to save, starting c4

--- src/orders/service/orders.service.ts
+++ src/orders/service/orders.service.ts
@@ -1,4 +1,50 @@
 import { Injectable } from '@nestjs/common';
 
+type Order = {
+  orderNumber: number;
+  item: string;
+  quantity: number;
+  pricePerUnit: number;
+};
+
 @Injectable()
-export class OrdersService {}
+export class OrdersService {
+  private orders: Order[];
+
+  constructor() {
+    this.orders = [];
+  }
+
+  // getOrders(): string[] {
+  //   return this.orders;
+  // }
+
+  // getOrderByName(customer: string): string {
+  //   if (!this.orders.includes(customer)) {
+  //     throw new NotFoundException(
+  //       `No user can be found with the name: ${customer}`,
+  //     );
+  //   }
+  //   return this.orders.find((element) => element === customer);
+  // }
+
+  // createOrder(customer: string): string {
+  //   this.orders.push(customer);
+  //   return customer;
+  // }
+
+  // updateOrder(customer: string, newOrder: string): string {
+  //   if (!this.orders.includes(customer)) {
+  //     throw new NotFoundException(
+  //       `No user can be found with the name: ${customer}`,
+  //     );
+  //   }
+  //   const index = this.orders.indexOf(customer);
+  //   this.orders[index] = newOrder;
+  //   return this.orders.find((element) => element === newOrder);
+  // }
+
+  // deleteOrder(customer: string): void {
+  //   this.orders = this.orders.filter((ordersItem) => ordersItem !== customer);
+  // }
+}
