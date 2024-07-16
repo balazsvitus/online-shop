1e6fae33482a5d08792a2ff9853e1b8466986ed7
Vitus Balazs <Balazs.Vitus@msg.group>
2024-07-16 14:37:34 +0300

    Added CRUD operations for Customer

--- src/customers/controller/customers.controller.ts
+++ src/customers/controller/customers.controller.ts
@@ -1,4 +1,47 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { CustomersService } from '../service/customers.service';
+
+type Customer = {
+  name: string;
+};
 
 @Controller('customers')
-export class CustomersController {}
+export class CustomersController {
+  constructor(private customerService: CustomersService) {}
+
+  @Get()
+  getCustomers(): string[] {
+    return this.customerService.getCustomers();
+  }
+
+  @Get(':name')
+  getCustomerByName(@Param() { name }: { name: string }): string {
+    return this.customerService.getCustomerByName(name);
+  }
+
+  @Post()
+  createCustomer(@Body() customerBody: Customer): string {
+    return this.customerService.createCustomer(customerBody.name);
+  }
+
+  @Put(':name')
+  updateCustomer(
+    @Param() { name }: { name: string },
+    @Body() customerBody: Customer,
+  ): string {
+    return this.customerService.updateCustomer(name, customerBody.name);
+  }
+
+  @Delete(':name')
+  deleteCustomer(@Param() { name }: { name: string }): void {
+    this.customerService.deleteCustomer(name);
+  }
+}
--- src/customers/service/customers.service.ts
+++ src/customers/service/customers.service.ts
@@ -1,4 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 
 @Injectable()
-export class CustomersService {}
+export class CustomersService {
+  private customers: string[];
+
+  constructor() {
+    this.customers = [];
+  }
+
+  getCustomers(): string[] {
+    return this.customers;
+  }
+
+  getCustomerByName(customer: string): string {
+    if (!this.customers.includes(customer)) {
+      throw new NotFoundException(
+        `No user can be found with the name: ${customer}`,
+      );
+    }
+    return this.customers.find((element) => element === customer);
+  }
+
+  createCustomer(customer: string): string {
+    this.customers.push(customer);
+    return customer;
+  }
+
+  updateCustomer(customer: string, newCustomer: string): string {
+    if (!this.customers.includes(customer)) {
+      throw new NotFoundException(
+        `No user can be found with the name: ${customer}`,
+      );
+    }
+    const index = this.customers.indexOf(customer);
+    this.customers[index] = newCustomer;
+    return this.customers.find((element) => element === newCustomer);
+  }
+
+  deleteCustomer(customer: string): void {
+    this.customers = this.customers.filter(
+      (customersItem) => customersItem !== customer,
+    );
+  }
+}
