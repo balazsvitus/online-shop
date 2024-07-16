acd2b837388f09edee4338a08afb43838d0b9cea
Vitus Balazs <Balazs.Vitus@msg.group>
2024-07-16 09:51:54 +0300

    Chapter 3: Created Nest.js app

--- index.js
+++
@@ -1,6 +0,0 @@
-console.log('hello world1');
-console.log('hello world2');
-let unusedVariable = 5;
-console.log('hello world3');
-const UNUSED_CONST = 'this is a string';
-console.log('hello world4');
